14d5ea1c7715b254a135005f7e5b704c7a75e9fc
shupri <100562812+shupri@users.noreply.github.com>
2023-04-11 21:31:41 +0700

    fungsi-calc

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,129 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
- return MaterialApp(
- title: 'Kalkulator',
- home: Scaffold(
- appBar: AppBar(
- title: Text('Kalkulator gwecj'),
- ),
- body: Padding(
-  padding: const EdgeInsets.all(20.0),
-  child: Column(
-    children: [
-      Text("Hasil : ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-      SizedBox(height: 20),
-      TextField(decoration: InputDecoration(labelText: "Angka pertama", border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),),
-      SizedBox(height: 20),
-      TextField(decoration: InputDecoration(labelText: "Angka kedua", border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))),),
-      SizedBox(height: 20),
-      ElevatedButton(onPressed: (){
+import 'package:flutter/services.dart';
 
-      }
-      , child: Text("/")),
-      SizedBox(height: 20),
-      ElevatedButton(onPressed: (){
+main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
 
-      }
-      , child: Text("*")),
-      SizedBox(height: 20),
-      ElevatedButton(onPressed: (){
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-      }
-      , child: Text("+")),
-      SizedBox(height: 20),
-      ElevatedButton(onPressed: (){
+class _MyAppState extends State<MyApp> {
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+  int? result = 0, num1 = 0, num2 = 0;
+  tambah() {
+    setState(() {
+      num1 = int.parse(controller1.text);
+      num2 = int.parse(controller2.text);
+      result = num1! + num2!;
+    });
+  }
 
-      }
-      , child: Text("-")),
-      SizedBox(height: 20),
-    ],
-  ),
- ),
- ),
- );
- }
+  kurang() {
+    setState(() {
+      num1 = int.parse(controller1.text);
+      num2 = int.parse(controller2.text);
+      result = num1! - num2!;
+    });
+  }
+
+  bagi() {
+    setState(() {
+      num1 = int.parse(controller1.text);
+      num2 = int.parse(controller2.text);
+      result = num1! ~/ num2!;
+    });
+  }
+
+  kali() {
+    setState(() {
+      num1 = int.parse(controller1.text);
+      num2 = int.parse(controller2.text);
+      result = num1! * num2!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Kalkulator gwejc"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(
+              "Hasil : $result",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: controller1,
+              decoration: InputDecoration(
+                  labelText: "Angka Pertama",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: controller2,
+              decoration: InputDecoration(
+                  labelText: "Angka Kedua",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20))),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      tambah();
+                      controller1.clear();
+                      controller2.clear();
+                    },
+                    child: Text("+")),
+                ElevatedButton(
+                    onPressed: () {
+                      kurang();
+                      controller1.clear();
+                      controller2.clear();
+                    },
+                    child: Text("-")),
+                ElevatedButton(
+                    onPressed: () {
+                      kali();
+                      controller1.clear();
+                      controller2.clear();
+                    },
+                    child: Text("x")),
+                ElevatedButton(
+                    onPressed: () {
+                      bagi();
+                      controller1.clear();
+                      controller2.clear();
+                    },
+                    child: Text("/"))
+                    
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
